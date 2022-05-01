02d6e0d13436f99cc89965296664553fbc4462a9
jason1039 <46224417+jason1039@users.noreply.github.com>
2022-05-01 20:04:40 +0800

    End

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] IDList = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+int[] Contactee = { 2, 0, 7, 11, 15, 4, 14, 1, 8, 13, 3, 5, 12, 9, 6, 10 };
+Console.WriteLine("Enter id of infected citizen:");
+string temp_str = Console.ReadLine();
+int InputPserson = int.Parse(temp_str);
+List<int> Result = new();
+while (!Result.Contains(InputPserson))
+{
+    Result.Add(InputPserson);
+    InputPserson = Contactee[InputPserson];
+}
+Console.WriteLine(String.Join(",", Result.ToArray()));
+Console.WriteLine("End");
