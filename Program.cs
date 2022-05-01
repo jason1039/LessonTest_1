3a67a43e11a8b2780abf8eb603830d22d0ed9120
jason1039 <46224417+jason1039@users.noreply.github.com>
2022-05-01 20:06:33 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -2,8 +2,7 @@
 int[] IDList = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 int[] Contactee = { 2, 0, 7, 11, 15, 4, 14, 1, 8, 13, 3, 5, 12, 9, 6, 10 };
 Console.WriteLine("Enter id of infected citizen:");
-string temp_str = Console.ReadLine();
-int InputPserson = int.Parse(temp_str);
+int InputPserson = 3;
 List<int> Result = new();
 while (!Result.Contains(InputPserson))
 {
